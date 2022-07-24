46329e9bdd22ee28a48e607be0bf32153f1dcbe7
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2022-07-24 16:08:22 +0100

    Add open graph meta tags

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,13 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Restaurant',
+      meta: {
+        'og:site_name': {property: 'og:site_name', content: 'davidcorcoran.ie'},
+        'og:title': {property: 'og:title', content: 'dall\'inferno'},
+        'og:image': {property: 'og:image', content: 'https://i.imgur.com/m3SVnlh.png'},
+        'og:description': {property: 'og:description', content: 'An example restaurant website for the Odin Project.'},
+        'og:url': {property: 'og:url', content: 'https://www.davidcorcoran.ie/odin-restaurant-page/'},
+      }
     }),
   ],
   devServer: {
